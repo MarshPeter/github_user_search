3141e5e3de87332b26a8180f9a8be3ee3c270dd5
MarshPeter <peter.m.farmer.204@gmail.com>
2025-11-03 19:51:52 +1100

    correctly reading arguments

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length == 0)
+        {
+            var versionString = Assembly.GetEntryAssembly()?
+                .GetCustomAttribute<AssemblyInformationalVersionAttribute>()?
+                .InformationalVersion
+                .ToString() ?? "";
+
+            if (string.IsNullOrEmpty(versionString))
+            {
+                Console.WriteLine("Program started outside of CLI, closing program");
+                return;
+            }
+
+            Console.WriteLine($"Github User Search v{versionString}");
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("\nUsage:");
+            Console.WriteLine("  github-activity <username>");
+
+            return;
+        }
+
+        ShowUsername(string.Join(" ", args));
+    }
+
+    static void ShowUsername(string username)
+    {
+        string message = $"Here is the username: {username}";
+
+        Console.WriteLine(message);
+    }
+}
